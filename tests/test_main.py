fc3cc18f6b52b7c0d65bf85cf43afdfa23dc724b
Maksim_ka <miheevm332@gmail.com>
2026-01-08 23:00:21 +0300

    new feat

--- tests/test_main.py
+++ tests/test_main.py
@@ -3,61 +3,65 @@ import unittest
 from src.main import Category, Product
 
 
-class TestProductCategory(unittest.TestCase):
+class TestProduct(unittest.TestCase):
+    def test_create_product(self):
+        product = Product("iPhone 14 Pro Max", "Серебряный корпус, 256 ГБ памяти", 120000.0, 10)
+        self.assertEqual(product.name, "iPhone 14 Pro Max")
+        self.assertEqual(product.description, "Серебряный корпус, 256 ГБ памяти")
+        self.assertEqual(product.price, 120000.0)
+        self.assertEqual(product.quantity, 10)
 
+    def test_price_setter_valid_value(self):
+        product = Product("iPhone 14 Pro Max", "Серебряный корпус, 256 ГБ памяти", 120000.0, 10)
+        product.price = 130000.0
+        self.assertEqual(product.price, 130000.0)
+
+    def test_new_product_class_method(self):
+        data = {
+            "name": "Samsung Galaxy Z Fold 4",
+            "description": "Складной экран, 512 ГБ памяти",
+            "price": 150000.0,
+            "quantity": 5,
+        }
+        product = Product.new_product(data)
+        self.assertIsInstance(product, Product)
+        self.assertEqual(product.name, "Samsung Galaxy Z Fold 4")
+        self.assertEqual(product.description, "Складной экран, 512 ГБ памяти")
+        self.assertEqual(product.price, 150000.0)
+        self.assertEqual(product.quantity, 5)
+
+
+class TestCategory(unittest.TestCase):
     def setUp(self):
-        # Сбрасываем счетчики перед каждым тестом для изоляции
-        Category.category_count = 0
-        Category.product_count = 0
+        self.category = Category("Смартфоны", "Каталог смартфонов")
+        self.product1 = Product("iPhone 14 Pro Max", "Серебряный корпус, 256 ГБ памяти", 120000.0, 10)
+        self.product2 = Product("Samsung Galaxy S23 Ultra", "Черный корпус, 512 ГБ памяти", 150000.0, 5)
 
-        # Создаем продукты для тестов
-        self.product1 = Product(
-            "Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5
-        )
-        self.product2 = Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
-        self.product3 = Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14)
-        self.product4 = Product('55" QLED 4K', "Фоновая подсветка", 123000.0, 7)
+    def test_add_product(self):
+        self.category.add_product(self.product1)
+        self.assertIn(self.product1, self.category.get_products())
+        self.assertEqual(len(self.category.get_products()), 1)
 
-    def test_product_attributes(self):
-        self.assertEqual(self.product1.name, "Samsung Galaxy S23 Ultra")
-        self.assertEqual(self.product1.description, "256GB, Серый цвет, 200MP камера")
-        self.assertEqual(self.product1.price, 180000.0)
-        self.assertEqual(self.product1.quantity, 5)
+    def test_get_products_list(self):
+        self.category.add_product(self.product1)
+        expected_output = ["iPhone 14 Pro Max, 120000.00 руб. Остаток: 10 шт."]
+        self.assertListEqual(expected_output, self.category.get_products_list().split("\n"))
 
-    def test_category_creation_and_counts(self):
-        category = Category(
-            "Смартфоны",
-            "Описание категории",
-            [self.product1, self.product2, self.product3],
-        )
-        self.assertEqual(category.name, "Смартфоны")
-        self.assertEqual(category.description, "Описание категории")
-        self.assertEqual(len(category.products), 3)
+    def test_category_count_and_product_count(self):
+        initial_categories = Category._category_count
+        initial_products = Category._product_count
 
-        # Проверяем счетчики класса
-        self.assertEqual(Category.category_count, 1)
-        self.assertEqual(Category.product_count, 3)
+        self.category.add_product(self.product1)
+        self.category.add_product(self.product2)
 
-    def test_multiple_categories_update_counts(self):
-        Category("Смартфоны", "Описание", [self.product1, self.product2])
-        Category("Телевизоры", "Описание", [self.product4])
-        self.assertEqual(Category.category_count, 2)
-        self.assertEqual(Category.product_count, 3)  # 2 + 1
+        self.assertEqual(Category._category_count, initial_categories)
+        self.assertEqual(Category._product_count, initial_products + 2)
 
-    def test_empty_products_list(self):
-        category_empty = Category("Пустая", "Без товаров", [])
-        self.assertEqual(len(category_empty.products), 0)
-        self.assertEqual(Category.product_count, 0)
-        self.assertEqual(Category.category_count, 1)
-
-    def test_counters_increment_properly(self):
-        initial_category_count = Category.category_count
-        initial_product_count = Category.product_count
-
-        Category("Новая", "Описание", [self.product3])
-
-        self.assertEqual(Category.category_count, initial_category_count + 1)
-        self.assertEqual(Category.product_count, initial_product_count + 1)
+    def test_get_products_returns_copy(self):
+        self.category.add_product(self.product1)
+        original_products = self.category.get_products()
+        original_products.pop()  # Модификация копии списка
+        self.assertNotEqual(original_products, self.category.get_products())  # Проверка, что оригинал не изменился
 
 
 if __name__ == "__main__":
